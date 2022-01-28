583fa60b14179d1a6872a4b576204aecd465b769
keeransethupathi <31564372+keeransethupathi@users.noreply.github.com>
2022-01-28 20:52:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from sqlmodel import Field, SQLModel
+import datetime
 
 
 class Pos(SQLModel, table=True):
     sentence: str = Field(default=None, primary_key=True)
+    date: datetime.datetime.now()
