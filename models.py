5350095c98f41fdc4be5e0f7317d30d453042a46
keeransethupathi <31564372+keeransethupathi@users.noreply.github.com>
2022-01-28 22:00:38 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,5 @@
 from sqlmodel import Field, SQLModel
-import datetime
 
 
 class Pos(SQLModel, table=True):
     sentence: str = Field(default=None, primary_key=True)
-    date: Field(sa_column=sqlmodel.Column(sqlmodel.DateTime(timezone=True),nullable=False))
