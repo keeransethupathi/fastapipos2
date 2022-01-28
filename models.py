bd526a92b98a1ff799172c00cf19d3badee81717
keeransethupathi <31564372+keeransethupathi@users.noreply.github.com>
2022-01-28 22:52:21 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
+import datetime
 from sqlmodel import Field, SQLModel
-
+from datetime import datetime, timedelta
 
 class Pos(SQLModel, table=True):
     sentence: str = Field(default=None, primary_key=True)
+    time: datetime = Field((datetime.utcnow() + timedelta(hours=5, minutes=30)).strftime('%d-%b-%y %H:%M:%S'))
