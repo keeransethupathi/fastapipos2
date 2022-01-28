093e73ccba4f029e1419b26b5bbde4e0affa846d
keeransethupathi <31564372+keeransethupathi@users.noreply.github.com>
2022-01-28 23:07:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,4 +4,4 @@ from datetime import datetime, timedelta
 
 class Pos(SQLModel, table=True):
     sentence: str = Field(default=None, primary_key=True)
-    time: datetime = Field((datetime.utcnow() + timedelta(hours=5, minutes=30)).strftime('%d-%b-%y %H:%M:%S'))
+    date: str = Field((datetime.utcnow() + timedelta(hours=5, minutes=30)).strftime('%d-%b-%y %H:%M:%S'))
